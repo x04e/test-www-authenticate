1bca9baa0ecf201501234d18da5fff902aef6bfb
Liam Hilliard <liam.h.-@hotmail.com>
2023-11-03 18:51:30 +0000

    Initial commit

---
+++ src/main/java/com/x04e/testwwwauthenticate/BasicAuthServlet.java
@@ -0,0 +1,19 @@
+package com.x04e.testwwwauthenticate;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class BasicAuthServlet {
+
+	public void init() throws Exception {
+		// Initialise
+	}
+
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws Exception {
+		// Handle GET request
+	}
+
+	public void destroy() {
+		// Destroy
+	}
+}
---
+++ src/test/java/com/x04e/testwwwauthenticate/ExampleTest.java
@@ -0,0 +1,12 @@
+package com.x04e.testwwwauthenticate;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+
+public class ExampleTest {
+	
+	@Test
+	void test(){
+		assertTrue(2 > 1);
+	}
+}
